3d64da2bd253f660f2331632f3b25c485e8fb0a3
Daniel Ayo <mrdenzo@live.co.uk>
2016-05-04 00:18:40 +0100

    Modifying Plugin Editor according to http://www.redwoodaudio.net/Tutorials/juce_for_vst_development__intro3.html

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,42 +1,115 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 4.2.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
 #include "PluginEditor.h"
 
 
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
 //==============================================================================
-AutomizerAudioProcessorEditor::AutomizerAudioProcessorEditor (AutomizerAudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+AutomizerAudioProcessorEditor::AutomizerAudioProcessorEditor (AutomizerAudioProcessor& ownerFilter)
+    : AudioProcessorEditor(ownerFilter)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    //[Constructor_pre] You can add your own custom stuff here..
+	startTimer(200); //starts timer with interval of 200ms
+    //[/Constructor_pre]
+
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (600, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
 }
 
 AutomizerAudioProcessorEditor::~AutomizerAudioProcessorEditor()
 {
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
 }
 
 //==============================================================================
 void AutomizerAudioProcessorEditor::paint (Graphics& g)
 {
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
     g.fillAll (Colours::white);
 
-    g.setColour (Colours::black);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
 }
 
 void AutomizerAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
 }
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+void AutomizerAudioProcessorEditor::timerCallback()
+{
+	AutomizerAudioProcessor* ourProcessor = getProcessor();
+	//exchange any data you want between UI elements and the Plugin "ourProcessor"
+}
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Projucer information section --
+
+    This is where the Projucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="AutomizerAudioProcessorEditor"
+                 componentName="" parentClasses="public AudioProcessorEditor, public Timer"
+                 constructorParams="AutomizerAudioProcessor&amp; ownerFilter"
+                 variableInitialisers="AudioProcessorEditor(ownerFilter)" snapPixels="8"
+                 snapActive="1" snapShown="1" overlayOpacity="0.330" fixedSize="0"
+                 initialWidth="600" initialHeight="400">
+  <BACKGROUND backgroundColour="ffffffff"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,40 +1,75 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 4.2.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#ifndef PLUGINEDITOR_H_INCLUDED
-#define PLUGINEDITOR_H_INCLUDED
+#ifndef __JUCE_HEADER_CCC97E6E72718693__
+#define __JUCE_HEADER_CCC97E6E72718693__
 
-#include "../JuceLibraryCode/JuceHeader.h"
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
 #include "PluginProcessor.h"
+//[/Headers]
+
 
 
 //==============================================================================
 /**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
-class AutomizerAudioProcessorEditor  : public AudioProcessorEditor
+class AutomizerAudioProcessorEditor  : public AudioProcessorEditor,
+                                       public Timer
 {
 public:
-    AutomizerAudioProcessorEditor (AutomizerAudioProcessor&);
+    //==============================================================================
+    AutomizerAudioProcessorEditor (AutomizerAudioProcessor& ownerFilter);
     ~AutomizerAudioProcessorEditor();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    //[UserMethods]     -- You can add your own custom methods in this section.
+	void timerCallback();
+	AutomizerAudioProcessor* getProcessor() const
+	{
+		return static_cast <AutomizerAudioProcessor*>(getAudioProcessor());
+	}
+	
+	//[/UserMethods]
+
+    void paint (Graphics& g) override;
     void resized() override;
 
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AutomizerAudioProcessor& processor;
 
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AutomizerAudioProcessorEditor)
 };
 
+//[EndFile] You can add extra defines here...
+//[/EndFile]
 
-#endif  // PLUGINEDITOR_H_INCLUDED
+#endif   // __JUCE_HEADER_CCC97E6E72718693__
